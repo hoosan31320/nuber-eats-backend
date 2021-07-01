fb03adddae0bbc0d5fad479528c241621d376795
hoosan A <hoos31320@gmail.com>
2021-07-02 00:33:59 +0700

    4.9.Log In part Two

--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -18,13 +18,14 @@ export class UsersResolver {
     @Args('input') createAccountInput: CreateAccountInput,
   ): Promise<CreateAccountOutput> {
     try {
-      const { ok, error } = await this.usersService.createAccount(
-        createAccountInput,
-      );
-      return {
-        ok,
-        error,
-      };
+      // const { ok, error } = await this.usersService.createAccount(
+      //   createAccountInput,
+      // );
+      // return {
+      //   ok,
+      //   error,
+      // };
+      return this.usersService.createAccount(createAccountInput);
     } catch (error) {
       return {
         ok: false,
@@ -34,5 +35,15 @@ export class UsersResolver {
   }
 
   @Mutation(returns => LoginOutput)
-  async login(@Args('input') loginInput: LoginInput) {}
+  async login(@Args('input') loginInput: LoginInput) {
+    try {
+      return this.usersService.login(loginInput);
+    } catch (error) {
+      return {
+        ok: false,
+        error,
+      };
+    }
+  }
+  
 }
