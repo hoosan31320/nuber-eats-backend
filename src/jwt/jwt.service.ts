9f90d9882a9a754cdeb577b557eecf6779923504
hoosan A <hoos31320@gmail.com>
2021-07-03 18:29:23 +0700

    Global ConfigMudule can

--- src/jwt/jwt.service.ts
+++ src/jwt/jwt.service.ts
@@ -1,13 +1,17 @@
 import { Inject, Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import * as jwt from 'jsonwebtoken';
 import { CONFIG_OPTIONS } from './jwt.constants';
 import { JwtModuleOptions } from './jwt.interfaces';
 
+
 @Injectable()
 export class JwtService {
-  constructor(@Inject(CONFIG_OPTIONS) private readonly options: JwtModuleOptions) {
-    console.log(options);
-  }
-  hello() {
-    console.log('hello');
+  constructor(
+    @Inject(CONFIG_OPTIONS) private readonly options: JwtModuleOptions,
+    private readonly configService: ConfigService                      // ConfigModule
+  ) {}                                                                 // 이 Global이라서
+  sign(payload: object): string {                                      // configService
+    return jwt.sign(payload, this.configService.get("PRIVATE_KEY"));   // 불러와서 처리 가능
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -14,9 +14,7 @@ export class UsersService {
     @InjectRepository(User) private readonly users: Repository<User>,
     private readonly config: ConfigService,
     private readonly jwtService: JwtService,
-  ) {
-    this.jwtService.hello();
-  }
+  ) {}
 
   async createAccount({ 
     email, 
@@ -55,7 +53,7 @@ export class UsersService {
           error: 'Wrong password',
         };
       }
-      const token = jwt.sign({ id: user.id }, this.config.get('SECRET_KEY'));
+      const token = this.jwtService.sign({id:user.id});
       return {
         ok: true,
         token,
