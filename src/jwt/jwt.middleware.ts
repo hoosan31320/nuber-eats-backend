caf9abd54ce1f330518c7b0c464a28009238ad65
hoosan A <hoos31320@gmail.com>
2021-07-03 22:21:32 +0700

    5.7 JWT Middleware

--- src/jwt/jwt.middleware.ts
+++ src/jwt/jwt.middleware.ts
@@ -1,6 +1,26 @@
-import { NextFunction } from "express";
+import { NextFunction, Request, Response } from "express";
+import { Injectable, NestMiddleware } from "@nestjs/common";
+import { JwtService } from "./jwt.service";
+import { UsersService } from "src/users/users.service";
 
-export function jwtMiddleware(req: Request, res: Response, next: NextFunction ) {
-  console.log(req.headers);
-  next();
+@Injectable()
+export class JwtMiddleware implements NestMiddleware {
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly UsersService: UsersService,
+  ) {}
+  async use(req: Request, res: Response, next: NextFunction) {
+    if ('x-jwt' in req.headers) {
+      const token = req.headers['x-jwt'];
+      const decoded = this.jwtService.verify(token.toString());
+      if (typeof decoded === 'object' && decoded.hasOwnProperty('id')) {
+        try {
+          const user = await this.UsersService.findById(decoded['id']);
+          req['user'] = user;
+        } catch (e) {}
+      } 
+    }
+    next();
+  }
+
 }
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,13 +1,12 @@
 import { Module } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { JwtService } from 'src/jwt/jwt.service';
 import { User } from './entities/user.entity';
 import { UsersResolver } from './users.resolver';
 import { UsersService } from './users.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],       //   ConfigModule.forRoot({    @Global()
-  providers: [UsersResolver, UsersService],          //     isGlobal: true,         export class JwtModule
+  providers: [UsersResolver, UsersService],         //     isGlobal: true,         export class JwtModule
+  exports: [UsersService],
 })
 export class UsersModule {}
