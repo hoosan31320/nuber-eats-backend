dc07e18afaad9e501c17a4d403a392c9505f9015
hoosan A <hoos31320@gmail.com>
2021-07-06 00:53:44 +0700

    10.1 Relationships and InputTypes

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,6 +11,7 @@ import { Verification } from './users/entities/verification.entity';
 import { MailModule } from './mail/mail.module';
 import { Restaurant } from './restaurants/entities/restaurant.entity';
 import { Category } from './restaurants/entities/category.entity';
+import { RestaurantsModule } from './restaurants/restaurants.module';
 
 
 @Module({
@@ -56,6 +57,7 @@ import { Category } from './restaurants/entities/category.entity';
       fromEmail: process.env.MAILGUN_FROM_EMAIL,
     }),
     UsersModule,
+    RestaurantsModule,
   ],
   controllers: [],
   providers: [],
