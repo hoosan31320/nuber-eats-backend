70282667cedd93a1ea48ebda18a4f2b2049e9cfd
hoosan A <hoos31320@gmail.com>
2021-07-06 18:42:41 +0700

    10.4 Roles part One

--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -1,17 +1,19 @@
 import { Resolver, Query, Args, Mutation, ArgsType } from "@nestjs/graphql";
 import { AuthUser } from "src/auth/auth-user.decorator";
+import { Role } from "src/auth/role.decorator";
 import { User } from "src/users/entities/user.entity";
 import { CreateRestaurantInput, CreateRestaurantOutput } from "./dtos/create-restaurant.dto";
 import { Restaurant } from "./entities/restaurant.entity";
 import { RestaurantService } from "./restaurants.service";
 
-@Resolver()
+@Resolver(of => Restaurant)
 
 export class RestaurantResolver {
   constructor(private readonly restaurantService: RestaurantService) {}
  
 
   @Mutation(returns => CreateRestaurantOutput)
+  @Role(['Owner'])
   async createRestaurant(
     @AuthUser() authUser: User,
     @Args('input') createRestaurantInput: CreateRestaurantInput,
