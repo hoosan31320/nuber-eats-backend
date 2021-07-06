27538832f62d5866ca5eb0723b85566f2bdf5cf1
hoosan A <hoos31320@gmail.com>
2021-07-06 13:11:08 +0700

    10.2 createRestaurant part One

--- src/restaurants/restaurants.service.ts
+++ src/restaurants/restaurants.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
+import { User } from "src/users/entities/user.entity";
 import { Repository } from "typeorm";
-import { CreateRestaurantDto } from "./dtos/create-restaurant.dto";
-import { UpdateRestaurantDto } from "./dtos/update-restaurant.dto";
+import { CreateRestaurantInput, CreateRestaurantOutput } from "./dtos/create-restaurant.dto";
 import { Restaurant } from "./entities/restaurant.entity";
 
 @Injectable()
@@ -11,19 +11,23 @@ export class RestaurantService {
     @InjectRepository(Restaurant)
     private readonly restaurants: Repository<Restaurant>,
   ) {}
-  
-  getAll(): Promise<Restaurant[]> {
-    return this.restaurants.find();
+
+  async createRestaurant(
+    owner: User,
+    createRestaurantInput: CreateRestaurantInput,
+  ): Promise<CreateRestaurantOutput> {
+    try {
+      const newRestaurant = this.restaurants.create(createRestaurantInput);
+      await this.restaurants.save(newRestaurant);
+      return {
+        ok: true,
+      };
+    } catch {
+      return {
+        ok: false,
+        error: 'Could not create reataurant',
+      }
+    }
   }
 
-  createRestaurant(
-    createRestaurantDto: CreateRestaurantDto,
-  ): Promise<Restaurant> {
-    const newRestaurant = this.restaurants.create(createRestaurantDto);  //create는 Restaurant을 return
-    return this.restaurants.save(newRestaurant);                         //save는 Promise를 return
-  }
-
-  updateRestaurant({ id, data }: UpdateRestaurantDto) {
-    return this.restaurants.update(id, { ...data});                      //update도 Promise를 return
-  }                                                                      //id가 존재하든 않하든 그냥 update(save)
-}                                                                        //id가 없으면 create만 돼고 DB에 저장은 않됌
+}  
