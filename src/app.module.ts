6905bf2470651bfee30e4510ff62cda624b5b9a8
hoosan A <hoos31320@gmail.com>
2021-07-10 18:21:12 +0700

    11.6 Order Items

--- src/app.module.ts
+++ src/app.module.ts
@@ -16,6 +16,7 @@ import { AuthModule } from './auth/auth.module';
 import { Dish } from './restaurants/entities/dish.entity';
 import { OrdersModule } from './orders/orders.module';
 import { Order } from './orders/entities/order.entity';
+import { OrderItem } from './orders/entities/order-item.entity';
 
 
 @Module({
@@ -46,7 +47,15 @@ import { Order } from './orders/entities/order.entity';
       database: process.env.DB_NAME,
       synchronize: process.env.NODE_ENV !== 'prod',
       logging: process.env.NODE_ENV !== 'prod',
-      entities: [User, Verification, Restaurant, Category, Order, Dish],
+      entities: [
+        User, 
+        Verification, 
+        Restaurant, 
+        Category, 
+        Dish,
+        Order,
+        OrderItem,
+      ],
     }),
     GraphQLModule.forRoot({
       autoSchemaFile: true,
