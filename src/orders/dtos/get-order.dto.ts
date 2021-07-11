2e26f4f24d86b0adb6cc3c831c35eb4ec256f7a2
hoosan A <hoos31320@gmail.com>
2021-07-11 21:31:55 +0700

    11.14 Edit Order

--- src/orders/dtos/get-order.dto.ts
+++ src/orders/dtos/get-order.dto.ts
@@ -1,6 +1,6 @@
 import { Field, InputType, ObjectType, PickType } from "@nestjs/graphql";
 import { CoreOutput } from "src/common/dtos/output.dto";
-import { Order, OrderStatus } from "../entities/order.entity";
+import { Order } from "../entities/order.entity";
 
 
 @InputType()
